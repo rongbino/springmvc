23cffd3b67c8bd00cf6ebc938b2d9297356b3cd4
Tommy Rong <tommy.rong.cn@gmail.com>
2017-03-13 21:33:53 +0800

    run the jsp in jetty

--- src/main/java/com/emotibot/wordcloud/controller/GeneralController.java
+++ src/main/java/com/emotibot/wordcloud/controller/GeneralController.java
@@ -1,12 +1,8 @@
 package com.emotibot.wordcloud.controller;
 
-import java.util.Hashtable;
-
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
